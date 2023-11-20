e5a49ba23f9fc4713db152bedf9266008327e2d3
Stuart MacDonald <stuartm.coding@gmail.com>
2023-11-20 18:58:31 -0500

    Improvements

--- spelling_bee.py
+++ spelling_bee.py
@@ -10,9 +10,11 @@ from more_itertools import distinct_permutations
 THIS = os.path.basename(__file__)
 THIS_VERSION = "0.1"
 
-DEFAULT_WORD_LIST_PATH = '/usr/share/dict/american-english'
+#DEFAULT_WORD_LIST_PATH = '/usr/share/dict/american-english'
+#DEFAULT_WORD_LIST_PATH = '/home/smacdonald/Downloads/english.txt'
+DEFAULT_WORD_LIST_PATH = '/home/smacdonald/Downloads/sowpods.txt'
 MIN_WORD_LENGTH = 4
-MAX_WORD_LENGTH = 15
+MAX_WORD_LENGTH = 11
 
 def load_word_list(path):
     with open(path, 'r') as file:
@@ -20,7 +22,7 @@ def load_word_list(path):
 
 def spelling_bee_generator(letters, min_length=MIN_WORD_LENGTH, max_length=MAX_WORD_LENGTH):
     def _spelling_bee_generator(letters, min_length, max_length):
-        seen = set()
+        #seen = set()
         orig_letters = ''.join(letters)
         letters = letters.lower()
         letters = [letters[0]] + sorted(letters[1:])
@@ -33,7 +35,10 @@ def spelling_bee_generator(letters, min_length=MIN_WORD_LENGTH, max_length=MAX_W
             #for combo in itertools.product(letters, repeat=min_length):
             # Generate upper (transposed lower) triangular combos
             # https://stackoverflow.com/a/56962698
+            num_combos = 0
+            num_perms = 0
             for combo in itertools.combinations_with_replacement(letters, min_length):
+                num_combos += 1
                 #print(f'-= it.product() = {combo}')
                 if combo[0] != letters[0]:
                     #print('-= BREAKING')
@@ -41,16 +46,18 @@ def spelling_bee_generator(letters, min_length=MIN_WORD_LENGTH, max_length=MAX_W
                 #if combo[-1] < combo[-2]:
                     #print("-= CONTINUING")
                 for perm in distinct_permutations(combo):
+                    num_perms += 1
                     word = ''.join(perm)
-                    if word in seen:
-                        seen_str = ', !!!!! SEEN !!!!!'
-                        print(f'-= distinct_permutations() = {perm}{seen_str}')
-                    else:
-                        seen.add(word)
-                        seen_str = ''
+                    #if word in seen:
+                        #seen_str = ', !!!!! SEEN !!!!!'
+                        #print(f'-= distinct_permutations() = {perm}{seen_str}')
+                    #else:
+                        #seen.add(word)
+                        #seen_str = ''
                     #print(f'-= distinct_permutations() = {perm}{seen_str}')
                     yield ''.join(perm)
             min_length += 1
+            print(f'-= combos {num_combos:,}, perms {num_perms:,}')
     return _spelling_bee_generator(letters, min_length, max_length)
 
 arg_parser = argparse.ArgumentParser(prog=THIS, description='Spelling Bee solver')
@@ -63,7 +70,7 @@ args = arg_parser.parse_args()
 #    print(f'{THIS}: Error: 7 letters expected, {n} letter{"s"[:n^1]} provided \'{args.letters}\'')
 #    sys.exit(1)
 
-words = load_word_list('/usr/share/dict/american-english')
+words = load_word_list(DEFAULT_WORD_LIST_PATH)
 
 #for word in spelling_bee_generator(args.letters, min_length=1, max_length=3):
 #for word in spelling_bee_generator(args.letters, min_length=1, max_length=8):
