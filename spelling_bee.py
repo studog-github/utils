3f3f04640cf01398b22c8121936070c877c053da
Stuart MacDonald <stuartm.coding@gmail.com>
2023-11-20 09:34:39 -0500

    Update

--- spelling_bee.py
+++ spelling_bee.py
@@ -12,7 +12,7 @@ THIS_VERSION = "0.1"
 
 DEFAULT_WORD_LIST_PATH = '/usr/share/dict/american-english'
 MIN_WORD_LENGTH = 4
-MAX_WORD_LENGTH = 7
+MAX_WORD_LENGTH = 15
 
 def load_word_list(path):
     with open(path, 'r') as file:
@@ -20,21 +20,35 @@ def load_word_list(path):
 
 def spelling_bee_generator(letters, min_length=MIN_WORD_LENGTH, max_length=MAX_WORD_LENGTH):
     def _spelling_bee_generator(letters, min_length, max_length):
+        seen = set()
+        orig_letters = ''.join(letters)
         letters = letters.lower()
         letters = [letters[0]] + sorted(letters[1:])
+        print(f'-= spelling_bee_generator(letters = {orig_letters} {"".join(letters)}, min = {min_length}, max = {max_length})')
         if min_length == 1:
             yield letters[0]
             min_length += 1
         while min_length <= max_length:
-            for combo in itertools.product(letters, repeat=min_length):
-                print(f'-= {combo}')
+            print(f'-= {min_length} < {max_length}')
+            #for combo in itertools.product(letters, repeat=min_length):
+            # Generate upper (transposed lower) triangular combos
+            # https://stackoverflow.com/a/56962698
+            for combo in itertools.combinations_with_replacement(letters, min_length):
+                #print(f'-= it.product() = {combo}')
                 if combo[0] != letters[0]:
-                    print('breaking')
+                    #print('-= BREAKING')
                     break
-                if combo[-1] < combo[-2]:
-                    print("continuing")
-                    continue
+                #if combo[-1] < combo[-2]:
+                    #print("-= CONTINUING")
                 for perm in distinct_permutations(combo):
+                    word = ''.join(perm)
+                    if word in seen:
+                        seen_str = ', !!!!! SEEN !!!!!'
+                        print(f'-= distinct_permutations() = {perm}{seen_str}')
+                    else:
+                        seen.add(word)
+                        seen_str = ''
+                    #print(f'-= distinct_permutations() = {perm}{seen_str}')
                     yield ''.join(perm)
             min_length += 1
     return _spelling_bee_generator(letters, min_length, max_length)
@@ -52,12 +66,16 @@ args = arg_parser.parse_args()
 words = load_word_list('/usr/share/dict/american-english')
 
 #for word in spelling_bee_generator(args.letters, min_length=1, max_length=3):
+#for word in spelling_bee_generator(args.letters, min_length=1, max_length=8):
+word_count = 0
 for word in spelling_bee_generator(args.letters):
     if word in words:
+        word_count += 1
         if len(set(word)) == 7:
             print(word, 'PANAGRAM')
         else:
             print(word)
+print(f'Found {word_count} words')
 
 #for word_len in range(MIN_WORD_LENGTH, MAX_WORD_LENGTH + 1):
 #    print(f'-= {word_len}')
